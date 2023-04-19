8e0ff07ace68904efdb9548ad07273e026d71f70
Omi1112 <omi1520135@gmail.com>
2023-04-20 01:00:10 +0900

    update: 会計一般ユーザを作成

--- iam/main.tf
+++ iam/main.tf
@@ -1,13 +1,24 @@
 data "aws_caller_identity" "current" {}
 
-# マネージャユーザの作成
+# 会計マネージャユーザの作成
 resource "aws_iam_user" "finance_manager" {
   name = "FinanceManager"
 }
 
-# Billingフルアクセス権限グループ
+# 会計一般ユーザの作成
+resource "aws_iam_user" "finance_user" {
+  name = "FinanceUser"
+}
+
+
+# Billingフルアクセスグループ
 resource "aws_iam_group" "billing_full_access_group" {
   name = "BillingFullAccessGroup"
+}
+
+# Billing閲覧グループ
+resource "aws_iam_group" "billing_view_access_group" {
+  name = "BillingViewAccessGroup"
 }
 
 # IP権限をグループポリシーへアタッチ
@@ -15,16 +26,32 @@ resource "aws_iam_group_policy_attachment" "billing_full_access_group_attachment
   policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/IPLimit"
   group      = aws_iam_group.billing_full_access_group.name
 }
-
-# Billingフルアクセス権限をBillingフルアクセス権限グループポリシーへアタッチ
-resource "aws_iam_group_policy_attachment" "billing_full_access_group_attachment_aws_billing_read_only_access" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSBillingReadOnlyAccess"
-  group      = aws_iam_group.billing_full_access_group.name
+resource "aws_iam_group_policy_attachment" "billing_view_access_group_attachment_ip_limit" {
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/IPLimit"
+  group      = aws_iam_group.billing_view_access_group.name
 }
 
 
-# マネージャユーザをグループに所属させる
+# Billingフルアクセス権限をBillingフルアクセスグループへアタッチ
+resource "aws_iam_group_policy_attachment" "billing_full_access_group_attachment_billing" {
+  policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
+  group      = aws_iam_group.billing_full_access_group.name
+}
+
+# Billing閲覧権限をBilling閲覧グループへアタッチ
+resource "aws_iam_group_policy_attachment" "billing_view_access_group_attachment_aws_billing_read_only_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSBillingReadOnlyAccess"
+  group      = aws_iam_group.billing_view_access_group.name
+}
+
+# 会計マネージャユーザをBillingフルアクセス権限グループに所属させる
 resource "aws_iam_user_group_membership" "finance_manager_belongs_to_billing_full_access_group" {
   user   = aws_iam_user.finance_manager.name
   groups = [aws_iam_group.billing_full_access_group.name]
+}
+
+# 会計一般ユーザをBilling閲覧権限グループに所属させる
+resource "aws_iam_user_group_membership" "finance_user_belongs_to_billing_view_access_group" {
+  user   = aws_iam_user.finance_user.name
+  groups = [aws_iam_group.billing_view_access_group.name]
 }
