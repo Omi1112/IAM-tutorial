e1586e7dca1b8245bdae25c8e314f633e5d5d330
Omi1112 <omi1520135@gmail.com>
2023-04-20 16:21:47 +0900

    add: IAM閲覧ユーザの作成

---
+++ iam/custom_policy.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_user" "policy_user" {
+  name = "PolicyUser"
+}
+
+resource "aws_iam_policy" "read_only_iam_console_policy" {
+  name        = "UsersReadOnlyAccessToIAMConsole"
+  path        = "/"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+            "iam:GenerateCredentialReport",
+            "iam:Get*",
+            "iam:List*"
+        ],
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+# IP権限をグループポリシーへアタッチ
+resource "aws_iam_user_policy_attachment" "policy_user_attachment_ip_limit" {
+  user       = aws_iam_user.policy_user.name
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/IPLimit"
+}
+
+resource "aws_iam_user_policy_attachment" "policy_user_attachment_read_only_iam_console_policy" {
+  user       = aws_iam_user.policy_user.name
+  policy_arn = aws_iam_policy.read_only_iam_console_policy.arn
+}
